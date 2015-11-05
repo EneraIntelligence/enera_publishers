21f4d3765710596f73307b7d85c9e1fa1de895e3
Alfonso Rodriguez <alfonso@vizinova.com>
2015-11-05 12:49:27 -0600

    Chart view preview finish

--- resources/views/profile/charts.blade.php
+++ resources/views/profile/charts.blade.php
@@ -1,9 +1,23 @@
 @extends('layouts.main')
+@section('head_scripts')
+    {!! HTML::style(asset('css/profile.css')) !!}
+    <style>
+       .md-chart{
+           margin: 0 25px;
+           padding: 10px 0;
+       }
+
+       .md-chart-card
+       {
+           padding: 20px 0;
+       }
+    </style>
+@endsection
 
 @section('content')
 
     <div id="page_content">
-        <div id="page_content_inner">
+        <div id="page_content_inner" style="padding-bottom: 25px;">
             <div class="uk-grid" data-uk-grid-margin data-uk-grid-match id="user_profile">
                 <div class="uk-width-large-1">
                     <div class="md-card">
@@ -19,67 +33,87 @@
                                 </div>
                             </div>
                             <div class="user_heading_avatar">
-                                <img src="assets/img/avatars/avatar_11.png" alt="user avatar"/>
+                                <img style="background-image:none!important;" id="img"
+                                     src="{!!URL::asset('images/avatar/'. $user->image )  !!}"
+                                     alt="User avatar"/>
                             </div>
                             <div class="user_heading_content">
                                 <h2 class="heading_b uk-margin-bottom"><span
-                                            class="uk-text-truncate">{!! $user->name !!}</span><span class="sub-heading">{!! $user->id !!}</span>
+                                            class="uk-text-truncate">{!! $user->name['first'] . ' ' .$user->name['last']!!}</span><span
+                                            class="sub-heading">{!! $user->id !!}</span>
                                 </h2>
                                 <ul class="user_stats">
                                     <li>
-                                        <h4 class="heading_a">{!! $campaign !!} <span class="sub-heading">Campañas</span></h4>
+                                        <h4 class="heading_a">{!! $campaign !!} <span
+                                                    class="sub-heading">Campañas</span></h4>
                                     </li>
                                     <li>
-                                        <h4 class="heading_a">{!! $closed !!} <span class="sub-heading">Terminadas</span></h4>
+                                        <h4 class="heading_a">{!! $closed !!} <span
+                                                    class="sub-heading">Terminadas</span></h4>
                                     </li>
                                     <li>
-                                        <h4 class="heading_a">{!! $canceled !!} <span class="sub-heading">Canceladas</span></h4>
+                                        <h4 class="heading_a">{!! $canceled !!} <span
+                                                    class="sub-heading">Canceladas</span></h4>
                                     </li>
                                 </ul>
                             </div>
                         </div>
                     </div>
-                    <div class="md-card" style=" margin: 0;">
-                        <div class="uk-grid" style=" background: white; margin: 0; padding: 10px" >
-                            <div class="uk-width-1-2">
-                                <h2>Datos Generales</h2>
+                </div>
+            </div>
+            <div>
+                <div class="uk-grid">
+                    <div class="uk-width-small-1 uk-width-medium-1-2 md-chart">
+                        <h2>Datos Generales</h2>
+                        <div class="md-card md-card-hover md-chart-card">
 
-                                <div id="chart1"></div>
-                            </div>
-                            <div class="uk-width-1-2">
-                                <h2>Campañas Termiandas</h2>
 
-                                <div id="chart2"></div>
-                            </div>
-                            <div class="uk-width-1-2">
-                                <h2>Caapañas Canceladas</h2>
+                            <div id="chart1"></div>
+                        </div>
+                    </div>
+                    <div class="uk-width-small-1 uk-width-medium-1-2 md-chart">
+                        <h2>Campañas Termiandas</h2>
+                        <div class="md-card md-card-hover md-chart-card">
 
-                                <div id="chart3"></div>
-                            </div>
-                            <div class="uk-width-1-2">
-                                <h2>Camapañas Pendientes</h2>
 
-                                <div id="chart4"></div>
-                            </div>
+                            <div id="chart2"></div>
+                        </div>
+                    </div>
+                    <div class="uk-width-small-1 uk-width-medium-1-2 md-chart">
+                        <h2>Capañas Canceladas</h2>
+                        <div class="md-card md-card-hover md-chart-card">
+
+
+                            <div id="chart3"></div>
+                        </div>
+                    </div>
+                    <div class="uk-width-small-1 uk-width-medium-1-2 md-chart">
+                        <h2>Camapañas Pendientes</h2>
+                        <div class="md-card md-card-hover md-chart-card">
+
+
+                            <div id="chart4"></div>
                         </div>
                     </div>
                 </div>
             </div>
+
         </div>
     </div>
 
 
-    <div id="page_content">
-        <div id="page_content_inner">
-            <div class="uk-grid" data-uk-grid-margin data-uk-grid-match id="user_profile">
-                <div class="uk-width-1">
-                    <div class="md-card md-card-content md-card-hover">
 
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    {{--<div id="page_content">--}}
+    {{--<div id="page_content_inner">--}}
+    {{--<div class="uk-grid" data-uk-grid-margin data-uk-grid-match id="user_profile">--}}
+    {{--<div class="uk-width-1">--}}
+    {{--<div class="md-card md-card-content md-card-hover">--}}
+
+    {{--</div>--}}
+    {{--</div>--}}
+    {{--</div>--}}
+    {{--</div>--}}
+    {{--</div>--}}
 @stop
 
 @section('scripts')
@@ -94,7 +128,7 @@
                     ['data3', 300],
                     ['data4', 50]
                 ],
-                type : 'donut'
+                type: 'donut'
             },
             color: {
                 pattern: ['red', '#aec7e8', '#ff7f0e', '#ffbb78', '#2ca02c', '#98df8a', '#d62728', '#ff9896', '#9467bd', '#c5b0d5', '#8c564b', '#c49c94', '#e377c2', '#f7b6d2', '#7f7f7f', '#c7c7c7', '#bcbd22', '#dbdb8d', '#17becf', '#9edae5']
