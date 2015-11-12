f903d934bdb8396ea10ffdc02e49a7ef91792a8a
Alfonso Rodriguez <alfonso@vizinova.com>
2015-11-12 10:20:15 -0600

    Analytic view

--- resources/views/analytics/single.blade.php
+++ resources/views/analytics/single.blade.php
@@ -1,15 +1,45 @@
 @extends('layouts.main')
 
 @section('content')
-    <div class="uk-grid uk-grid-collapse">
-        <div class="uk-width-small-1-2">
-            <h3>Generos</h3>
-            <div id="chart1">
-            </div>
+
+    <div id="page_content">
+        <div id="page_content_inner" style="padding-bottom: 25px;">
+                <div class="uk-grid uk-grid-small">
+                    <div class="uk-width-small-1 uk-width-medium-1-2 md-chart">
+                        <h2>Datos Generales</h2>
+                        <div class="md-card md-card-hover md-chart-card">
+
+
+                            <div id="chart1"></div>
+                        </div>
+                    </div>
+                    <div class="uk-width-small-1 uk-width-medium-1-2 md-chart">
+                        <h2>Campañas Termiandas</h2>
+                        <div class="md-card md-card-hover md-chart-card">
+
+
+                            <div id="chart2"></div>
+                        </div>
+                    </div>
+                    <div class="uk-width-small-1 uk-width-medium-1-2 md-chart">
+                        <h2>Capañas Canceladas</h2>
+                        <div class="md-card md-card-hover md-chart-card">
+
+
+                            <div id="chart3"></div>
+                        </div>
+                    </div>
+                    <div class="uk-width-small-1 uk-width-medium-1-2 md-chart">
+                        <h2>Camapañas Pendientes</h2>
+                        <div class="md-card md-card-hover md-chart-card">
+
+
+                            <div id="chart4"></div>
+                        </div>
+                    </div>
+                </div>
         </div>
     </div>
-
-
 @stop
 
 
@@ -69,8 +99,56 @@
             }
         });
 
-        $("#phone_number").kendoMaskedTextBox({
-            mask: "(99) 0000-0000"
+        var chart3 = c3.generate({
+            bindto: '#chart3',
+            data: {
+                columns: [
+                    ['data1', 30, 200, 100, 400, 150, 250],
+                    ['data2', 50, 20, 10, 40, 15, 25]
+                ],
+                axes: {
+                    data1: 'y',
+                    data2: 'y2'
+                }
+            },
+            color: {
+                pattern: ['red', '#aec7e8', '#ff7f0e', '#ffbb78', '#2ca02c', '#98df8a', '#d62728', '#ff9896', '#9467bd', '#c5b0d5', '#8c564b', '#c49c94', '#e377c2', '#f7b6d2', '#7f7f7f', '#c7c7c7', '#bcbd22', '#dbdb8d', '#17becf', '#9edae5']
+            },
+            axis: {
+                y: {
+                    padding: {top: 200, bottom: 0}
+                },
+                y2: {
+                    padding: {top: 100, bottom: 100},
+                    show: true
+                }
+            }
+        });
+
+        var chart4 = c3.generate({
+            bindto: '#chart4',
+            data: {
+                columns: [
+                    ['data1', 30, 200, 100, 400, 150, 250],
+                    ['data2', 50, 20, 10, 40, 15, 25]
+                ],
+                axes: {
+                    data1: 'y',
+                    data2: 'y2'
+                }
+            },
+            color: {
+                pattern: ['red', '#aec7e8', '#ff7f0e', '#ffbb78', '#2ca02c', '#98df8a', '#d62728', '#ff9896', '#9467bd', '#c5b0d5', '#8c564b', '#c49c94', '#e377c2', '#f7b6d2', '#7f7f7f', '#c7c7c7', '#bcbd22', '#dbdb8d', '#17becf', '#9edae5']
+            },
+            axis: {
+                y: {
+                    padding: {top: 200, bottom: 0}
+                },
+                y2: {
+                    padding: {top: 100, bottom: 100},
+                    show: true
+                }
+            }
         });
     </script>
 @stop
