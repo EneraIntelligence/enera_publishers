afe8b15c08b4c3c5a7707710f5b90762b68836f5
Alfonso Rodriguez <alfonso@vizinova.com>
2015-12-08 12:53:19 -0600

    Added new middleware in kernel

---
+++ app/Http/Middleware/PreviewMiddleware.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Publishers\Http\Middleware;
+
+use Auth;
+use Closure;
+use Publishers\Administrator;
+
+class PreviewMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $route = \Request::route()->getName();
+        $user = Administrator::where('_id', Auth::user()->_id)->first();
+        $diff = $user->route;
+        if ($route !=  "home" && $route != "campaigns::show")
+            array_unshift($diff, $route);
+        if (count($diff) > 5) {
+            array_pop($diff);
+        }
+        $user->route = $diff;
+        $user->save();
+        return $next($request);
+    }
+}
