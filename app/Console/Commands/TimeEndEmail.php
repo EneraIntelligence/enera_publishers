a5851cb0fcc5e846e459e9cfc322f46754fc9b0f
Alfonso Rodriguez <alfonso@vizinova.com>
2015-12-11 13:09:01 -0600

    email notification for ended campaign

--- app/Console/Commands/TimeEndEmail.php
+++ app/Console/Commands/TimeEndEmail.php
@@ -3,6 +3,7 @@
 namespace Publishers\Console\Commands;
 
 use Auth;
+use Exception;
 use Illuminate\Console\Command;
 use Mail;
 use MongoDate;
@@ -49,32 +50,36 @@ class TimeEndEmail extends Command
     public function handle()
     {
 
+        try {
+            $this->info('------------ Mandando Correo de Notificación -------------');
+            $today = date('Y-m-d');
+            $campaings = Campaign::where('status', 'active')
+                ->whereRaw([
+                    'filters.date.end' => [
+                        '$lt' => new MongoDate(strtotime($today))
+                    ]
+                ])
+                ->get();
 
-        $this->info('------------ Mandando Correo de Notificación -------------');
-        $today = date('Y-m-d');
-        $campaings = Campaign::where('status', 'active')
-            ->whereRaw([
-            'filters.date.end' => [
-                '$lt' => new MongoDate(strtotime($today))
-            ]
-        ])
-            ->get();
+            foreach ($campaings as $key => $cam) {
+                $cam->status = 'ended';
+                $cam->save();
+                $cam->history()->create(array('administrator_id' => '0', 'status' => 'ended', 'date' => $today, 'note' => 'Campaña finalizada por fecha de terminación'));
 
-        foreach ($campaings as $key => $cam) {
-            $cam->status = 'ended';
-            $cam->save();
-            $cam->history()->create(array('administrator_id' => '0', 'status' => 'ended', 'date' => $today, 'note' => 'Campaña finalizada por fecha de terminación'));
+                $user = Administrator::find($cam->administrator_id);
 
-            $user = Administrator::find($cam->administrator_id);
+                Mail::send('emails.notifications', ['user' => $user], function ($m) use ($user) {
+                    $m->from('soporte@enera.mx', 'Enera Intelligence');
+                    $m->to($user->email, $user->name['first'] . ' ' . $user->name['last'])->subject('Terminacion de Camapaña');
+                });
+                $this->info('             Correo enviado  ' . $user->email . '              ');
+            }
+            $this->info('-------------------- Fin de comando ----------------------');
 
-            Mail::send('emails.notifications', ['user' => $user], function ($m) use ($user) {
-                $m->from('soporte@enera.mx', 'Enera Intelligence');
-                $m->to('darkdreke@gmail.com', $user->name['first'] . ' ' . $user->name['last'])->subject('Terminacion de Camapaña');
-            });
-            $this->info('             Correo enviado  ' . $user->email . '              ');
+        }catch (Exception $e)
+        {
+            $this->error($e);
         }
-        $this->info('-------------------- Fin de comando ----------------------');
-
     }
 
 }
