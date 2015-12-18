3b6e2cc08831079d63a9655113e673b4b60120bb
Alfonso Rodriguez <alfonso@vizinova.com>
2015-12-18 10:10:14 -0600

    fix icon in menu main.blade

--- resources/views/emails/creation.blade.php
+++ resources/views/emails/creation.blade.php
@@ -25,34 +25,98 @@
             </li>
             <li>
                 <div class="md-list-content">
-                    <span class="md-list-heading">Items: </span>
-                    <br>
-                    @foreach($camp->content['items'] as $content)
-                        <span class="md-list-heading">  - {{$content}}</span>
-                        <br>
-                    @endforeach
+                    <span class="md-list-heading">Tipo de interacción</span>
+                    <span class="md-list-heading"> - {{$camp->interaction['name']}}</span>
                 </div>
             </li>
             <li>
                 <div class="md-list-content">
-                    <span class="md-list-heading">Images: </span>
-                    <br>
-                    @foreach($camp->content['images'] as $content)
-                        <span class="md-list-heading">  - {{$content}}</span>
-                        <br>
-                    @endforeach
+                    <span class="md-list-heading">Fecha de creacion</span>
+                    <span class="md-list-heading"> - {{$camp->created_at}}</span>
                 </div>
             </li>
-            <li>
-                <div class="md-list-content">
-                    <span class="md-list-heading">Images: </span>
-                    <br>
-                    @foreach($camp->content['survey'] as $content)
-                        <span class="md-list-heading">  - {{$content}}</span>
+            @if(isset($camp->content['items']))
+                <li>
+                    <div class="md-list-content">
+                        <span class="md-list-heading">Items: </span>
                         <br>
-                    @endforeach
-                </div>
-            </li>
+
+                        @foreach($camp->content['items'] as $content)
+                            <span class="md-list-heading">  - {{$content}}</span>
+                            <br>
+                        @endforeach
+
+                    </div>
+                </li>
+            @endif
+            @if(isset($camp->content['images']))
+                <li>
+                    <div class="md-list-content">
+                        <span class="md-list-heading">Images: </span>
+                        <br>
+                        @foreach($camp->content['images'] as $key => $content)
+                            <span class="md-list-heading">  - {{$key .' : ' . $content}}</span>
+                            <br>
+                        @endforeach
+
+                    </div>
+                </li>
+            @endif
+            @if(isset($camp->content['survey']))
+                <li>
+                    <div class="md-list-content">
+                        <span class="md-list-heading">Encuesta: </span>
+                        <br>
+                        @foreach($camp->content['survey'] as $content)
+                            <span class="md-list-heading">  - {{ $content}}</span>
+                            <br>
+                            @foreach($content['answers'] as $a )
+                                <span class="md-list-heading">  - {{ $a}}</span>
+                                <br>
+                            @endforeach
+                        @endforeach
+                    </div>
+                </li>
+            @endif
+            @if(isset($camp->content['mail']))
+                <li>
+                    <div class="md-list-content">
+                        <span class="md-list-heading">Correo: </span>
+                        <br>
+                        @foreach($camp->content['mail'] as $content)
+                            <span class="md-list-heading">  - {{$content}}</span>
+                            <br>
+                        @endforeach
+                    </div>
+                </li>
+            @endif
+            @if(isset($camp->content['captcha']))
+                <li>
+                    <div class="md-list-content">
+                        <span class="md-list-heading">Captcha: </span>
+                        <br>
+
+                        @foreach($camp->content['captcha'] as $content)
+                            <span class="md-list-heading">  - {{$content}}</span>
+                            <br>
+                        @endforeach
+
+                    </div>
+            @endif
+            @if(isset($camp->content['video']))
+                <li>
+                    <div class="md-list-content">
+                        <span class="md-list-heading">Video: </span>
+                        <br>
+
+                        @foreach($camp->content['video'] as $content)
+                            <span class="md-list-heading">  - {{$content}}</span>
+                            <br>
+                        @endforeach
+
+                    </div>
+                </li>
+            @endif
         </ul>
     </div>
 </div>
