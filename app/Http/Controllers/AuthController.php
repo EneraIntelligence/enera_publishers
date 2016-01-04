3657641455fe2e64d606ba9f2407c758d04f5e43
Pedro Luna <pluna@enera.mx>
2016-01-04 11:40:09 -0600

    Fix AuthController > login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -35,7 +35,7 @@ class AuthController extends Controller
             'password' => 'required|alpha_num|min:8|max:16',
         ]);
         if ($validator->passes()) {
-            if (auth()->attempt(['email' => Input::get('email'), 'password' => Input::get('password')], Input::get('login_page_stay_signed'))) {
+            if (auth()->attempt(['email' => Input::get('email'), 'password' => Input::get('password'), 'status' => 'active'], Input::get('login_page_stay_signed'))) {
                 return redirect()->route('home');
             } else {
                 return redirect()->route('auth.index')->with('error', 'El correo y/o la contraseña son incorrectos.');
@@ -70,35 +70,35 @@ class AuthController extends Controller
 //            'estado' => 'required|max:250|alpha',
 //            'ciudad' => 'required|max:250|alpha'
         ]);
-/**     despues de las validaciones    **/
+        /**     despues de las validaciones    **/
         if ($validator->passes()) {
 //            $confirmation_code = str_random(30);
             $confirmation_code = Hash::make(Input::get('email'));
 //            $data['aleatorio'] = uniqid(); //Genera un id único para identificar la cuenta a traves del correo.
             $password = Hash::make(Input::get('password')); //encrypta la contraseña
-/***+*****  consulta que guarda el documento en mongo *************/
-            $newAdmin= Administrator::create(array('name' => ['first' => Input::get('nombre'), 'last' => Input::get('apellido')],
+            /***+*****  consulta que guarda el documento en mongo *************/
+            $newAdmin = Administrator::create(array('name' => ['first' => Input::get('nombre'), 'last' => Input::get('apellido')],
                 'email' => Input::get('email'), 'password' => $password,
                 'rol_id' => 'usuario', 'status' => 'pending'
             ));
             //                'location'=>['country'=>'mexico','state'=>Input::get('estado'),'city'=>Input::get('ciudad')],
 
 //            dd($newAdmin);
-/********** si el usuario se creo se llama el job para mandarle el correo de confirmacion ***************/
-            if($newAdmin){
+            /********** si el usuario se creo se llama el job para mandarle el correo de confirmacion ***************/
+            if ($newAdmin) {
                 echo 'se guardo el usuario';
                 $Token = validationCode::create(array(
-                    'administrator_id'=> $newAdmin->_id,'type'=>'validationEmail', 'token'=>$confirmation_code
+                    'administrator_id' => $newAdmin->_id, 'type' => 'validationEmail', 'token' => $confirmation_code
                 ));
-                if($Token->count() > 0){
+                if ($Token->count() > 0) {
                     echo 'se guardo la tabla de token';
                 }
                 //se  crea un array con los datos que se ocupan para formar el correo
-                $data['nombre']=Input::get('nombre');
-                $data['apellido']=Input::get('apellido');
-                $data['email']=Input::get('email');
-                $data['id_usuario']=$newAdmin->_id;  //usuario
-                $data['confirmation_code']=$confirmation_code;  //codigo generado para validar el correo
+                $data['nombre'] = Input::get('nombre');
+                $data['apellido'] = Input::get('apellido');
+                $data['email'] = Input::get('email');
+                $data['id_usuario'] = $newAdmin->_id;  //usuario
+                $data['confirmation_code'] = $confirmation_code;  //codigo generado para validar el correo
 //                dd($data);
                 //se llama el job mandar correo confirmacion
                 $this->dispatch(new newAdminJob([
@@ -117,22 +117,22 @@ class AuthController extends Controller
         return redirect()->route('auth.index')->with('registro', 'registrar');
     }
 
-    public function verify($id,$token)
+    public function verify($id, $token)
     {
         echo 'verificando <br>';
-        $code = ValidationCode::where('administrator_id','=',$id)->first();// busco el codigo en la base de datos
-        if($code->count() > 0){ //si se encuentra el usuario
-            if($code->token==$token){  //verifico si el token es el mismo
-                $admin= Administrator::where('_id','=',$id)->first();
+        $code = ValidationCode::where('administrator_id', '=', $id)->first();// busco el codigo en la base de datos
+        if ($code->count() > 0) { //si se encuentra el usuario
+            if ($code->token == $token) {  //verifico si el token es el mismo
+                $admin = Administrator::where('_id', '=', $id)->first();
                 $admin->status = 'active';   //cambio el status de pending a active
                 $admin->save();
                 //  se regresa al login con mensaje de que se activo cuenta
                 return redirect()->route('auth.index')->with('data', 'active');
-            }else{
-                return redirect()->route('auth.index')->with('data','invalido');
+            } else {
+                return redirect()->route('auth.index')->with('data', 'invalido');
             }
-        }else{
-            return redirect()->route('auth.index')->with('data','invalido');
+        } else {
+            return redirect()->route('auth.index')->with('data', 'invalido');
         }
 //        return 'verificando';
 
