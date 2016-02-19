f1ab7df107a780aab61c9748a3613ebc9211da76
Pedro Luna <pluna@enera.mx>
2016-02-19 17:17:26 -0600

    [NEW] IssueTrackerHelper

--- app/Issue.php
+++ app/Issue.php
@@ -8,11 +8,12 @@ class Issue extends Model
 {
     protected $fillable = ['msg', 'platform', 'environment', 'url', 'file', 'exception', 'responsible_id',
         'priority', 'status', 'history', 'session_vars'];
+    protected $dates = ['created_at', 'updated_at'];
 
     // relations
     public function reponsible()
     {
-        return $this->belongsTo('Portal\Administrator', 'responsible_id');
+        return $this->belongsTo('Publishers\Administrator', 'responsible_id');
     }
     // end relations
 }
