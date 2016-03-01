17550c16fa135ee319c050213bf10767a36543ff
Jose Asdrubal Rosas Matus <arosas@enera.mx>
2016-03-01 17:56:36 -0600

    se cambio la vista del error 503

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,47 +1,52 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Be right back.</title>
+<!doctype html>
+<!--[if lte IE 9]> <html class="lte-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 9]><!--> <html lang="en"> <!--<![endif]-->
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="initial-scale=1.0,maximum-scale=1.0,user-scalable=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Remove Tap Highlight on Windows Phone IE -->
+    <meta name="msapplication-tap-highlight" content="no"/>
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+    {{--<link rel="icon" type="image/png" href="assets/img/favicon-16x16.png" sizes="16x16">
+    <link rel="icon" type="image/png" href="assets/img/favicon-32x32.png" sizes="32x32">--}}
+    <link rel="icon" type="image/png" href="{!! URL::asset('images/favicon.png') !!}" sizes="32x32">
 
-        <style>
-            html, body {
-                height: 100%;
-            }
+    <title>error inesperado</title>
 
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                color: #B0BEC5;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
+    <link href='http://fonts.googleapis.com/css?family=Roboto:300,400,500' rel='stylesheet' type='text/css'>
 
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
+    <!-- uikit -->
+    <link rel="stylesheet" href="{!! URL::asset('bower_components/uikit/css/uikit.almost-flat.min.css') !!}"/>
 
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
+    <!-- altair admin error page -->
+    <link rel="stylesheet" href="{!! URL::asset('assets/css/error_page.min.css') !!}" />
 
-            .title {
-                font-size: 72px;
-                margin-bottom: 40px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Be right back.</div>
+</head>
+<body class="error_page">
+
+<div class="uk-width-1-1">
+    <div class="error_page_header">
+        <div class="uk-width-8-10 uk-container-center">
+            500!
+        </div>
+    </div>
+    <div class="uk-width-1-1 ">
+        <div class="uk-width-6-10 uk-container-center" style="float: left">
+            <div class="uk-width-8-10 uk-container-center">
+                <p class="heading_b">Oops! algo salio mal...</p>
+                <p class="uk-text-large">
+                    hubo un error. Intenta mas tarde.
+                </p>
+                <a href="#" onclick="history.go(-1);return false;">regresar a la pagina anterior</a>
             </div>
         </div>
-    </body>
+        <div class="uk-width-4-10  md-card-content" style="float: right">
+            <img src="{!! URL::asset('images/publisher.png') !!}">
+        </div>
+    </div>
+</div>
+
+
+</body>
 </html>
