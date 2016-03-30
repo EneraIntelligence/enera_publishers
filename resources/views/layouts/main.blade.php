e7daba174cbc7644f3da048ba9a6d932b99b2d84
Jose Asdrubal Rosas Matus <arosas@enera.mx>
2016-03-30 11:29:37 -0600

    se cambio la ruta del favicon

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -12,7 +12,7 @@
 
     {{--<link rel="icon" type="image/png" href="assets/img/favicon-16x16.png" sizes="16x16">
     <link rel="icon" type="image/png" href="assets/img/favicon-32x32.png" sizes="32x32">--}}
-    <link rel="icon" type="image/png" href="{!! URL::asset('img/favicon.png') !!}" sizes="32x32">
+    <link rel="icon" type="image/png" href="{!! URL::asset('images/favicon.png') !!}" sizes="32x32">
     <title>Enera Admins @yield('title')</title>
     @yield('head_scripts')
 
