f173eefd2a34c9f0e36bd9ac0fce3bc77de8f45d
Pedro Luna <pluna@enera.mx>
2016-04-25 16:20:07 -0500

    [FIX] Handler (issuetracker)

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -50,7 +50,6 @@ class Handler extends ExceptionHandler
         $debug = env('APP_DEBUG');
         if ($debug == 0) {
             if ($this->isHttpException($e)) {
-                IssueTrackerHelper::create($request, $e, 'Publishers');
                 return $this->renderHttpException($e);
             } else if ($e instanceof NotFoundHttpException) {
                 return response()->view('error.404', [], 404);
