2333655fdafc68c341d5921294840d99372ad62e
Eder Díaz <ederchrono@gmail.com>
2016-09-15 11:17:26 -0500

    hotfix on campaign close

--- app/Console/Commands/TimeEndEmail.php
+++ app/Console/Commands/TimeEndEmail.php
@@ -62,6 +62,9 @@ class TimeEndEmail extends Command
                 ])
                 ->get();
 
+
+            $count=0;
+
             foreach ($campaigns as $key => $cam) {
                 $camBalance = $cam->balance;
                 $cam->history()->create(array('administrator_id' => $cam->administrator_id, 'status' => 'ended', 'date' => $today, 'note' => 'Campaña finalizada por fecha de terminación'));
@@ -71,19 +74,19 @@ class TimeEndEmail extends Command
                 if ($admin) {
                     $admin->wallet->increment('current', $camBalance['current']);
                     $cam->history()->create(array('administrator_id' => $cam->administrator_id, 'status' => 'returned', 'date' => $today, 'note' => 'Balance restante se regreso a los fondos del cliente por la cantidad de $' . number_format($camBalance['current'], 2, '.', ',')));
-                    $user = Administrator::find($cam->administrator_id);
+                    //$user = Administrator::find($cam->administrator_id);
 
 //                Mail::send('emails.notifications', ['user' => $user], function ($m) use ($user)
 //                {
 //                    $m->from('soporte@enera.mx', 'Enera Intelligence');
 //                    $m->to($user->email, $user->name['first'] . ' ' . $user->name['last'])->subject('Terminacion de Camapaña');
 //                });
-                    $key += 1;
-                    $this->info('             Correo # ' . $key . ' enviado  ' . $user->email . '              ');
+                    $count += 1;
+                    $this->info('             Correo # ' . $count . ' enviado  ' . $admin->email . '              ');
                     $cam->status = 'ended';
                     $camBalance['current'] = 0;
                     $cam->balance = $camBalance;
-//                    $cam->save();
+                    $cam->save();
                 }
             }
             $this->info('-------------------- Fin de comando ----------------------');
